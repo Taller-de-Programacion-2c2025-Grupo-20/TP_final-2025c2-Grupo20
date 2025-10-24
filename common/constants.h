a154c556eb95b657eef2f4f4b5f62a4758be1bee
gonzadom <97991084+gonzadom@users.noreply.github.com>
2025-10-24 15:04:55 -0300

    subo archivos common del TP threads

---
+++ common/constants.h
@@ -0,0 +1,29 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+struct Instruction {
+    uint8_t command;
+    int client_id;
+
+    Instruction(): command(0x0), client_id(-1) {}
+    Instruction(uint8_t command, int client_id): command(command), client_id(client_id) {}
+};
+
+struct ResponseDTO {
+    uint8_t command;
+    uint16_t cars_with_nitro;
+
+    uint8_t nitro_state;
+
+    ResponseDTO(): command(0x0), cars_with_nitro(0), nitro_state(0) {}
+
+    ResponseDTO(uint8_t command, uint16_t cars_with_nitro, uint8_t nitro_state):
+            command(command), cars_with_nitro(cars_with_nitro), nitro_state(nitro_state) {}
+};
+
+#define SEND_NITRO 0x04
+#define SEND_CARS_WITH_NITRO 0x10
+#define NITRO_ACTIVATED 0x07
+#define NITRO_DEACTIVATED 0x08
+
+#endif

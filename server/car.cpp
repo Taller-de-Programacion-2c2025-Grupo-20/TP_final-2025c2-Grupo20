10b0937175e9fcc45e5a09544c0044766fec4306
gonzadom <97991084+gonzadom@users.noreply.github.com>
2025-10-26 18:39:13 -0300

    Agrego a server la clase Car con toda la logica de como mover un auto

---
+++ server/car.cpp
@@ -0,0 +1,137 @@
+#include "car.h"
+
+b2Vec2 Car::getLateralVelocity() {
+    b2Vec2 rightNormal = car_body->GetWorldVector(b2Vec2(1,0));
+    return b2Dot(rightNormal, car_body->GetLinearVelocity()) * rightNormal;
+}
+
+b2Vec2 Car::getForwardVelocity() {
+    b2Vec2 forwardNormal = car_body->GetWorldVector(b2Vec2(0,1));
+    return b2Dot(forwardNormal, car_body->GetLinearVelocity()) * forwardNormal;
+}
+
+void Car::limitSpeed() {
+    b2Vec2 fwdVel = getForwardVelocity();
+    float speed = fwdVel.Length();
+    if (speed > max_speed) {
+        fwdVel *= max_speed / speed;
+        b2Vec2 lateral = getLateralVelocity();
+        car_body->SetLinearVelocity(fwdVel + lateral);
+    }
+}
+
+void Car::updateFriction() {
+    float lateralFrictionFactor = 0.2f;
+    float angularDamping = 0.05f;
+
+    b2Vec2 lateralVel = getLateralVelocity();
+    b2Vec2 lateralImpulse = -(lateralFrictionFactor * car_body->GetMass()) * lateralVel;
+    car_body->ApplyLinearImpulse(lateralImpulse, car_body->GetWorldCenter());
+
+    car_body->SetAngularVelocity(car_body->GetAngularVelocity() * (1.0f - angularDamping));
+}
+
+void Car::handleInput(InputCmd cmd) {
+
+    if (cmd.key == InputKey::Up){
+        if (cmd.action == InputAction::Press){
+            accelerating = true;
+        }
+
+        if (cmd.action == InputAction::Release){
+            accelerating = false;
+        }
+    }
+
+    if (cmd.key == InputKey::Down){
+        if (cmd.action == InputAction::Press){
+            braking = true;
+        }
+
+        if (cmd.action == InputAction::Release){
+            braking = false;
+        }
+    }
+
+    if (cmd.key == InputKey::Left){
+        if (cmd.action == InputAction::Press){
+            turningLeft = true;
+        }
+
+        if (cmd.action == InputAction::Release){
+            turningLeft = false;
+        }
+    }
+
+    if (cmd.key == InputKey::Right){
+        if (cmd.action == InputAction::Press){
+            turningRight = true;
+        }
+
+        if (cmd.action == InputAction::Release){
+            turningRight = false;
+        }
+    }
+
+}
+
+void Car::updateCarPhysics() {
+    b2Vec2 forward = car_body->GetWorldVector(b2Vec2(0,1));
+    b2Vec2 force(0,0);
+    float torque = 0.f;
+
+    // Aplico fuerza de aceleración o frenado
+    if (accelerating) { 
+        force = accelaration * forward;
+    } 
+    
+    else if (braking) { 
+        force = -accelaration * forward;
+    }
+
+    else {
+        // Aplico freno si no hay input, por rozamiento con el piso
+        b2Vec2 fwdVel = getForwardVelocity();
+        float brakingFactor = 0.01f;
+        b2Vec2 brakeImpulse = -(brakingFactor * car_body->GetMass()) * fwdVel;
+        car_body->ApplyLinearImpulse(brakeImpulse, car_body->GetWorldCenter());
+    }
+
+    car_body->ApplyForce(force, car_body->GetWorldCenter());
+
+    // Gira solo si hay velocidad longitudinal
+    b2Vec2 fwdVel = getForwardVelocity();
+    float forwardSpeed = fwdVel.Length();
+    if (forwardSpeed > 1.5f) {
+        if (turningLeft)  torque = rotation_torque;
+        if (turningRight) torque = -rotation_torque;
+        car_body->ApplyTorque(torque);
+    }
+
+    // Elimino velocidad lateral y limito velocidad
+    updateFriction();
+    limitSpeed();
+}
+
+const b2Vec2& Car::position() {
+    return car_body->GetPosition();
+}
+
+float Car::angle() {
+    return car_body->GetAngle();
+}
+
+Car::Car(b2World& world, const b2Vec2& initial_position) {
+    b2BodyDef carDef;
+    carDef.type = b2_dynamicBody;
+    carDef.position.Set(initial_position.x, initial_position.y);
+    carDef.angle = 0.f;
+    carDef.angularDamping = 1.0f;
+    carDef.linearDamping = 0.1f;
+    
+    car_body = world.CreateBody(&carDef);
+    
+    b2PolygonShape car_shape; 
+    car_shape.SetAsBox(0.5f, 1.f);
+    car_body->CreateFixture(&car_shape,1.f);
+}
---
+++ server/car.h
@@ -0,0 +1,47 @@
+#ifndef CAR_H
+#define CAR_H
+
+#include <Box2D/Box2D.h>
+
+#include "clientCommand.h"
+#include "constants.h"
+
+#include <string>
+
+class Car {
+private:
+
+    float accelaration = 50.f;
+    float rotation_torque = 5.f;
+    float max_speed = 10.f;
+
+    bool accelerating = false;
+    bool braking = false;
+    bool turningLeft = false;
+    bool turningRight = false;
+
+    b2Body* car_body = nullptr;
+
+    b2Vec2 getLateralVelocity();
+
+    b2Vec2 getForwardVelocity();
+
+    void limitSpeed();
+
+    void updateFriction();
+
+public:
+
+    void handleInput(InputCmd cmd);
+
+    void updateCarPhysics();
+
+    const b2Vec2& position();
+
+    float angle();
+
+    Car(b2World& world, const b2Vec2& initial_position);
+};
+
+
+#endif

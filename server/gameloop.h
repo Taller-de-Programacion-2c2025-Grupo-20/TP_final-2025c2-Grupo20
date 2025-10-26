4e345a74f6282131b8bc55d8ef481b0d8ae2ca1d
gonzadom <97991084+gonzadom@users.noreply.github.com>
2025-10-26 18:39:51 -0300

    Gameloop suma el atributo world que va a contener la fisica del mundo

--- server/gameloop.h
+++ server/gameloop.h
@@ -3,28 +3,27 @@
 
 #include <unordered_map>
 
-#include "../common_src/queue.h"
-#include "../common_src/thread.h"
+#include <Box2D/Box2D.h>
+
+#include "../common/queue.h"
+#include "../common/thread.h"
+#include "../common/clientCommand.h"
 
 #include "queues_monitor.h"
 
 class Gameloop: public Thread {
 private:
-    Queue<Instruction>& gameloop_queue;
+    Queue<InputCmd>& gameloop_queue;
     QueuesMonitor& clients_queues;
 
-    std::unordered_map<int, int> clients_with_nitro;
-
-    void processInstruction(const Instruction& ins);
-
-    void updateClientsNitroState();
+    b2World world;
 
 public:
     void run() override;
 
     void stop() override;
 
-    Gameloop(Queue<Instruction>& gameloop_queue, QueuesMonitor& clients_queues);
+    Gameloop(Queue<InputCmd>& gameloop_queue, QueuesMonitor& clients_queues);
 };
 
 

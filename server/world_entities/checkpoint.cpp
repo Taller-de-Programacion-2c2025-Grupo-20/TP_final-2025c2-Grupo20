8c374ebfb63acf921c6aa403a80ed0f1da44e02f
gonzadom <97991084+gonzadom@users.noreply.github.com>
2025-11-11 14:14:46 -0300

    agrego la entidad del mundo checkpoint

---
+++ server/world_entities/checkpoint.cpp
@@ -0,0 +1,19 @@
+#include "checkpoint.h"
+
+Checkpoint::Checkpoint(b2World& world, const b2Vec2& position, float width, float height, int id) : id(id) {
+    b2BodyDef bodyDef;
+    bodyDef.type = b2_staticBody;
+    bodyDef.position = position;
+
+    checkpoint_body = world.CreateBody(&bodyDef);
+    checkpoint_body->SetUserData( this );
+
+    b2PolygonShape shape;
+    shape.SetAsBox(width, height);
+
+    b2FixtureDef fixtureDef;
+    fixtureDef.isSensor = true; //con esto hago que no sea solido
+    fixtureDef.shape = &shape;
+
+    checkpoint_body->CreateFixture(&fixtureDef);
+}
---
+++ server/world_entities/checkpoint.h
@@ -0,0 +1,28 @@
+#ifndef CHECKPOINT_H
+#define CHECKPOINT_H
+
+#include <Box2D/Box2D.h>
+
+#include "entity.h"
+
+#include "../../common/clientCommand.h"
+#include "../../common/constants.h"
+
+#include <string>
+
+class Checkpoint : public Entity {
+private:
+
+    int id;
+
+    b2Body* checkpoint_body;
+
+public:
+
+    EntityType getType() const override { return EntityType::CHECKPOINT; }
+
+    Checkpoint(b2World& world, const b2Vec2& position, float width, float height, int id);
+};
+
+
+#endif

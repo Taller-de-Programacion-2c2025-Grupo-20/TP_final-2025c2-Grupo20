dbc45ea5d5151f20c2acee62e6f146034ce9d2ec
gonzadom <97991084+gonzadom@users.noreply.github.com>
2025-11-18 00:10:55 -0300

    elimino todos los archivos que fueron subidos erroneamente al repositorio

--- _deps/sdl2_mixer-build/CMakeFiles/SDL2_mixer.dir/src/codecs/music_gme.c.o.d
+++
@@ -1,3 +0,0 @@
-_deps/sdl2_mixer-build/CMakeFiles/SDL2_mixer.dir/src/codecs/music_gme.c.o: \
- /home/gero/Documents/Taller/TP3/TP_final-2025c2-Grupo20/_deps/sdl2_mixer-src/src/codecs/music_gme.c \
- /usr/include/stdc-predef.h

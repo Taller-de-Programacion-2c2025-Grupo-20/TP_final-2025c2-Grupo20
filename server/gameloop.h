5109cf4da0d4eafa810df01fea772f9e9b2bfe57
gonzadom <97991084+gonzadom@users.noreply.github.com>
2025-11-23 12:04:56 -0300

    devuelvo el gameloop a su estado anterior con algunos cambios menores

--- server/gameloop.h
+++ server/gameloop.h
@@ -1,28 +1,27 @@
-#ifndef GAMELOOP_H_
-#define GAMELOOP_H_
+#ifndef GAMELOOP_H
+#define GAMELOOP_H
 
-#include <map>
-#include <mutex>
 #include <memory>
+#include <mutex>
+#include <unordered_map>
 #include <vector>
-#include <stdint.h>
 
 #include <Box2D/Box2D.h>
 #include <yaml-cpp/yaml.h>
 
-#include "../common/thread.h"
-#include "../common/queue.h"
 #include "../common/clientCommand.h"
-#include "../common/gameState.h"
-
-#include "queues_monitor.h"
+#include "../common/queue.h"
+#include "../common/thread.h"
 #include "world_entities/car.h"
-#include "world_entities/wall.h"
 #include "world_entities/checkpoint.h"
+#include "world_entities/wall.h"
+
 #include "collisions_listener.h"
+#include "queues_monitor.h"
 
 struct PlayerPos {
     float x, y;
+
     PlayerPos(float x, float y): x(x), y(y) {}
 };
 
@@ -30,40 +29,43 @@ class Gameloop: public Thread {
 private:
     Queue<InputCmd>& gameloop_queue;
     QueuesMonitor& clients_queues;
-    b2World& world;
 
-    std::mutex mutex;
-    std::map<uint8_t, std::unique_ptr<Car>> clients_cars;
-
+    std::unordered_map<uint8_t, std::unique_ptr<Car>> clients_cars;
     std::vector<std::unique_ptr<Wall>> world_walls;
-    std::vector<std::unique_ptr<Checkpoint>> world_checkpoints;
-    std::vector<PlayerPos> cars_initial_pos;
+    std::unordered_map<int, std::unique_ptr<Checkpoint>> world_checkpoints;
+    std::vector<PlayerPos> cars_inital_pos;
 
-    CollisionsListener collisions_listener;
+    b2World world;
+    CollisionsListener collision_listener;
 
     std::chrono::steady_clock::time_point start_time;
 
+    std::mutex mutex;
+
     void handleInput(const InputCmd& input);
+
     void loadWalls(const YAML::Node& map_data);
     void loadCheckpoints(const YAML::Node& map_data);
     void loadInitialPos(const YAML::Node& map_data);
+
     void loadMapData();
+
     void readUsersInput();
 
     float getCurrentCheckpointHintAngle(const b2Vec2& car_pos, float car_angle,
                                         const b2Vec2& checkpoint_pos);
 
-    GameStateDTO getCurrentGameState(float elapsed_time);
+    GameStateDTO getCurrentGameState(const float elapsed_time);
 
 public:
-    Gameloop(Queue<InputCmd>& gameloop_queue,
-             QueuesMonitor& clients_queues,
-             b2World& world);
+    void addCar(uint8_t client_id);
 
     void run() override;
+
     void stop() override;
 
-    void add_player(uint8_t player_id);
+    Gameloop(Queue<InputCmd>& gameloop_queue, QueuesMonitor& clients_queues);
 };
+
 
 #endif

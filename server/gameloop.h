690d134379a515b40a0f368a684a5c3e0473ca22
gonzadom <97991084+gonzadom@users.noreply.github.com>
2025-11-29 20:08:46 -0300

    En gameloop.cpp y .h cambio como se guarda la informacion de los autos para volver a agregarlos al mapa: - Guardo tambien la penalizacion pendiente por pagas en la sig carrera - Cuando construyo un auto puede ser sin penalizacion pendiente o con eso - Agrego que al registrar en la tabla de scores solo para aquellos que terminaron se les incremente el tiempo de finalizacion

--- server/gameloop.h
+++ server/gameloop.h
@@ -34,11 +34,12 @@ struct PlayerRaceInfo {
     bool timed_out;
 };
 
-struct DeletedCar {
+struct CarInfo {
     uint8_t player_id;
     CarType type;
+    float next_race_time_penalty;
 
-    DeletedCar(uint8_t player_id, CarType type) : player_id(player_id), type(type) {}
+    CarInfo(uint8_t player_id, CarType type, float next_race_time_penalty) : player_id(player_id), type(type), next_race_time_penalty(next_race_time_penalty) {}
 };
 
 class Gameloop: public Thread {
@@ -57,7 +58,7 @@ private:
     std::vector<PlayerRaceInfo> not_finished_results;
     uint8_t next_finish_position;
 
-    std::vector<DeletedCar> deleted_cars;
+    std::vector<CarInfo> deleted_cars;
 
     int current_race_number;
 
@@ -65,6 +66,8 @@ private:
     CollisionsListener collision_listener;
 
     std::chrono::steady_clock::time_point start_time;
+
+    void addCarWithTimePenalty(uint8_t client_id, const CarType& car_type, float time_penalty);
 
     void handleInput(const InputCmd& input);
 

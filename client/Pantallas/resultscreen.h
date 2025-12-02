09a24c84064a959df6e1d2e8eef79f0f2ac10d7a
nachozzm <zanoninacho05@gmail.com>
2025-12-02 00:13:49 -0300

    pantallas finales

--- client/Pantallas/resultscreen.h
+++ client/Pantallas/resultscreen.h
@@ -2,9 +2,11 @@
 #define RESULTSCREEN_H
 
 #include <QWidget>
+#include <QPixmap>
+#include <QPainter> 
 #include <vector>
 #include <map>
-#include "../common/gameState.h"
+#include "../common/dto.h"
 
 namespace Ui {
 class ResultScreen;
@@ -21,11 +23,19 @@ public:
                     const std::map<uint8_t, QString>& idToNameMap,
                     uint8_t myPlayerId);
 
+protected:
+    void paintEvent(QPaintEvent *event) override;
+
 private slots:
     void on_btnExit_clicked();
 
+signals:
+    void returnToLobby(); 
+    void closeApplication();
+
 private:
     Ui::ResultScreen *ui;
+    QPixmap backgroundImage; 
 };
 
 #endif // RESULTSCREEN_H
